2db3c7d9fdf61151b24a598ad9fbc7c25fa43be0
SoftwareEngineer <veliko@scbw.com>
2017-11-20 20:14:42 +0200

    fixed extra gradle

--- www/ooyala.js
+++ www/ooyala.js
@@ -1,16 +1,14 @@
-cordova.define("cordova-plugin-ooyala.ooyala", function(require, exports, module) {
 
-    var exec = require('cordova/exec');
-    var channel = require('cordova/channel');
+var exec = require('cordova/exec');
+var channel = require('cordova/channel');
 
-    module.exports = {
+module.exports = {
 
-        createPlayer: function (code, ecode, url, title, success, error) {
-            exec(success, error, "Ooyala", "createPlayer", [code, ecode, url, title]);
-        },
-        initPlayer: function (success, error) {
-            exec(success, error, "Ooyala", "initPlayer", []);
-        },
-    };
+    createPlayer: function (code, ecode, url, title, success, error) {
+        exec(success, error, "Ooyala", "createPlayer", [code, ecode, url, title]);
+    },
+    initPlayer: function (success, error) {
+        exec(success, error, "Ooyala", "initPlayer", []);
+    },
+};
 
-});
